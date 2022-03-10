cc0a24430bdaf13d7aeacbdfac3954c4407f9a49
sudarshan-magaji <sudarshan.s.magaji@gmail.com>
2022-03-10 19:31:02 +0530

    Added fields list.

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, Input, Output
+import dash_bootstrap_components as dbc
 import dash_cytoscape as cyto
 import pandas as pd
 
-app = Dash(__name__)
+app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 data=pd.read_excel('Extract2.xlsx')
 cvs=data.groupby(['OBJECT_NAME'])
@@ -30,15 +31,32 @@ final_nodes=get_nodes_dict(nodes)
 final_edges=get_edges_dict(edges)
 output_elements=final_nodes+final_edges
 
-app.layout = html.Div([
+def get_fields_list(node_name):
+    return list(cv_claim[['TARGET_NODE','TARGET_VALUE']].groupby('TARGET_NODE').get_group(node_name)['TARGET_VALUE'])
+
+card = dbc.Card(
+    dbc.ListGroup(id='fields_list',
+        flush=True,
+    ),
+    style={'position':'fixed','top':0,'right':0,"width": "15rem"},
+)
+
+app.layout = html.Div([dbc.Col([
     cyto.Cytoscape(
-        id='cytoscape-two-nodes',
+        id='cyto',
         layout={'name': 'breadthfirst',
         		'roots': '[id = "logicalModel"]'},
-        style={'width': '100%', 'height': '630px'},
+        style={'width': '80%', 'height': '630px'},
         elements=output_elements
-    )
+    ),
+    card])
 ])
+
+@app.callback(
+    Output('fields_list', 'children'),
+    Input('cyto', 'tapNodeData'))
+def populate_fields(node):
+    return [dbc.ListGroupItem(field) for field in get_fields_list(node['label'])]
 
 if __name__ == '__main__':
     app.run_server(debug=True)
