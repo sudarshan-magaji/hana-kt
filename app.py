a5250fed62aecf3482bbab3d680eda4c2c571d8e
sudarshan-magaji <sudarshan.s.magaji@gmail.com>
2022-03-15 15:36:50 +0530

    Final commit

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
 from dash import Dash, html, dcc, Input, Output, ALL
+import dash
 import dash_bootstrap_components as dbc
 import dash_cytoscape as cyto
 import pandas as pd
+from dash.exceptions import PreventUpdate
 
 app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 data=pd.read_excel('Extract2.xlsx')
 cvs=data.groupby(['OBJECT_NAME'])
 cv_claim=cvs.get_group('CV_CLAIM')
-cv_claim['SOURCE_NODE']=cv_claim['SOURCE_NODE'].fillna('')
+#cv_claim['SOURCE_NODE']=cv_claim['SOURCE_NODE'].fillna('')
 cv_claim['SOURCE_NODE']=cv_claim['SOURCE_NODE'].replace('#','',regex=True)
-cv_claim['TARGET_NODE']=cv_claim['TARGET_NODE'].fillna('')
-edges=list(cv_claim.groupby(['SOURCE_NODE','TARGET_NODE']).groups.keys())[2:]
-nodes=list(pd.concat([cv_claim['SOURCE_NODE'],cv_claim['TARGET_NODE']]).unique())
-nodes.remove('')
+#cv_claim['TARGET_NODE']=cv_claim['TARGET_NODE'].fillna('')
+#cv_claim=cv_claim[~cv_claim['TARGET_VALUE'].astype(str).str.contains('JOIN\$',regex=True)]
+edges=list(cv_claim[['TARGET_NODE','SOURCE_NODE']].drop_duplicates(['TARGET_NODE','SOURCE_NODE']).dropna().apply(lambda x: (x['TARGET_NODE'],x['SOURCE_NODE']),axis=1))
+nodes=cv_claim[['TARGET_NODE','SOURCE_NODE']].drop_duplicates(['TARGET_NODE','SOURCE_NODE']).dropna().stack().unique()
 
 target=(cv_claim['TARGET_NODE']+'+'+cv_claim['TARGET_VALUE'])
 source=(cv_claim['SOURCE_NODE']+'+'+cv_claim['SOURCE_VALUE'])
@@ -52,14 +54,13 @@ def get_fields_list(node_name):
 
 
 
-CYTO_STYLESHEET=[{'selector':'node','style':{'padding':'50%','content':'data(label)','text-valign': 'center','text-halign': 'center','height':100,'width':300,'shape':'cut-rectangle','font-size':'30px','text-wrap':'wrap'},},
+CYTO_STYLESHEET=[{'selector':'node','style':{'padding':'30px','content':'data(label)','text-valign': 'center','text-halign': 'center','height':'20px','width':'label','shape':'cut-rectangle','font-size':'30px','text-wrap':'wrap'},},
 
-                     {'selector':'edge','style':{'curve-style':'straight','width':'3px','arrow-shape':'vee'}}]
+                     {'selector':'edge','style':{'curve-style':'taxi','width':'3px','arrow-shape':'vee'}}]
 
 
 card = dbc.Card(
-    dbc.ListGroup(id='fields_list',
-        flush=True,
+    dcc.Dropdown(id='fields_list',placeholder='Select a node'
     ),
     style={'position':'fixed','top':0,'right':0,"width": "15rem"},
 )
@@ -68,7 +69,7 @@ app.layout = html.Div([dbc.Col([
     cyto.Cytoscape(
         id='cyto',
         layout={'name': 'breadthfirst',
-        		'roots': '[id = "logicalModel"]'},
+        		'roots': '[id = "logicalModel"]','grid':'true','spacingFactor':1},
         style={'width': '80%', 'height': '630px'},
         stylesheet=CYTO_STYLESHEET,
         elements=output_elements
@@ -77,20 +78,26 @@ app.layout = html.Div([dbc.Col([
 ])
 
 @app.callback(
-    Output('fields_list', 'children'),
+    Output('fields_list', 'options'),
     Input('cyto', 'tapNodeData'),prevent_initial_call=True)
 def populate_fields(node):
+    print(node['label'])
     try:
-        fields=[dbc.ListGroupItem(field,id={'type':'list-item','name':node['label']+'+'+field}) for field in get_fields_list(node['label'])]
-    except Exception as exc:
-        return None
-    return fields
+        fields=get_fields_list(node['label'])
+        return [{'label':field,'value':node['label']+'+'+field} for field in fields]
+    except KeyError:
+        return []
 
 @app.callback(
     Output('cyto', 'stylesheet'),
-    Input({'type': 'list-item', 'name': ALL}, 'n_clicks'),prevent_initial_call=True)
-def highlight_path(child):
-    pass
+    Input('fields_list', 'value'),prevent_initial_call=True)
+def highlight_path(value):
+    path=find_lineage(value)
+    path=[i.split('+')[0] for i in path]
+    node_selector=', '.join(["[id='{}']".format(i) for i in path])
+    stylesheet=CYTO_STYLESHEET[:]
+    stylesheet.append({'selector':node_selector,'style':{'background-color':'#F0E614'}})
+    return stylesheet
 
 
 if __name__ == '__main__':
